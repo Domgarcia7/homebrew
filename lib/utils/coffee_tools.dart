8461155f54afae45f2bd8730075b51b6b19d6566
Domgarcia7 <109365253+Domgarcia7@users.noreply.github.com>
2023-08-13 23:28:18 -0500

    coffee_tools done

--- lib/utils/coffee_tools.dart
+++ lib/utils/coffee_tools.dart
@@ -1,9 +1,32 @@
 class CoffeeTools {
+  double gramsOfWater = 0;
+  String coffeeType;
+  double coffeeAmount;
+
+  void setCoffeetype(String coffee) {
+    if (coffee == "French Press") {
+      coffeeType = "Course ground coffee";
+    } else {
+      coffeeType = "Medium ground coffee";
+    }
+  }
+
+  void convertWater(String number) {
+    double convert = double.parse(number);
+    gramsOfWater = 177.42 * convert;
+
+    if (coffeeType == "Course ground coffee") {
+      coffeeAmount = gramsOfWater / 14;
+    } else {
+      coffeeAmount = gramsOfWater / 17;
+    }
+  }
+
   // Input: number of cups (positive integer)
   // Output: number of ounces in a cup
   static int cupsToOunces(int cups) {
     if (cups <= 0) {
-      throw ArgumentError();
+      throw ArgumentError('Number of cups must be positive');
     }
     return cups * 6;
   }
