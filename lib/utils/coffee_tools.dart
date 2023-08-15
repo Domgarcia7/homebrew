dc0646ad7556a395ca5c56c9fd05775d72ab19bd
Domgarcia7 <109365253+Domgarcia7@users.noreply.github.com>
2023-08-15 16:21:56 -0500

    updated coffee tools file

--- lib/utils/coffee_tools.dart
+++ lib/utils/coffee_tools.dart
@@ -1,38 +1,33 @@
 class CoffeeTools {
-  double gramsOfWater = 0;
-  String coffeeType;
-  double coffeeAmount;
-
-//Determines the type of cofee based on the input string coffee 
-//and sets the coffeetype variable 
-  void setCoffeetype(String coffee) {
-    if (coffee == "French Press") {
-      coffeeType = "Course ground coffee";
-    } else {
-      coffeeType = "Medium ground coffee";
-    }
-  }
-
-//calculates the amount of water in grams based on the user input 
-//and then calculates the amount of coffee that should be used depending 
-//on the coffee type
-  void convertWater(String number) {
-    double convert = double.parse(number);
-    gramsOfWater = 177.42 * convert;
-
-    if (coffeeType == "Course ground coffee") {
-      coffeeAmount = gramsOfWater / 14;
-    } else {
-      coffeeAmount = gramsOfWater / 17;
-    }
-  }
-
   // Input: number of cups (positive integer)
   // Output: number of ounces in a cup
   static int cupsToOunces(int cups) {
     if (cups <= 0) {
-      throw ArgumentError('Number of cups must be positive');
+      throw ArgumentError();
     }
     return cups * 6;
+  }
+
+  static double ozToGrams(double oz) {
+    if (oz <= 0) {
+      throw ArgumentError();
+    }
+    return oz * 28.3495231;
+  }
+
+  static double ratioCoffeeFP(double ozOfWater) {
+    if (ozOfWater <= 0) {
+      throw ArgumentError();
+    } else {
+      return ozOfWater / 14.0;
+    }
+  }
+
+  static double ratioCoffeeDM(double ozOfWater) {
+    if (ozOfWater <= 0) {
+      throw ArgumentError();
+    } else {
+      return ozOfWater / 17.0;
+    }
   }
 }
