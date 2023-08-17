c8241d8e0fc42a24b6ff3a82082dcfcc56adb5f1
Domgarcia7 <109365253+Domgarcia7@users.noreply.github.com>
2023-08-16 23:09:43 -0500

    tests finished

--- test/coffee_tools_test.dart
+++ test/coffee_tools_test.dart
@@ -18,7 +18,53 @@ void main() {
     });
 
     test('throws ArgumentError on negative number', () {
-      expect(() => CoffeeTools.cupsToOunces(0), throwsArgumentError);
+      expect(() => CoffeeTools.cupsToOunces(-1), throwsArgumentError);
+    });
+  });
+
+  group("ozToGrams", () {
+    test('calculates grams from ounces', () {
+      var grams = CoffeeTools.ozToGrams(1);
+      expect(grams, closeTo(28.3495231, 0.0001));
+    });
+
+    test('throws ArgumentError on zero', () {
+      expect(() => CoffeeTools.ozToGrams(0), throwsArgumentError);
+    });
+
+    test('throws ArgumentError on negative number', () {
+      expect(() => CoffeeTools.ozToGrams(-2), throwsArgumentError);
+    });
+  });
+
+  group("ratioCoffeeFP", () {
+    test('calculates coffee-to-water ratio for FP', () {
+      var ratio = CoffeeTools.ratioCoffeeFP(14.0);
+      expect(ratio, closeTo(1.0, 0.0001));
+    });
+
+    test('throws ArgumentError on zero', () {
+      expect(() => CoffeeTools.ratioCoffeeFP(0), throwsArgumentError);
+    });
+
+    test('throws ArgumentError on negative number', () {
+      expect(() => CoffeeTools.ratioCoffeeFP(-2), throwsArgumentError);
+    });
+  });
+
+  group("ratioCoffeeDM", () {
+    test('calculates coffee-to-water ratio for DM', () {
+      var ratio = CoffeeTools.ratioCoffeeDM(17.0);
+      expect(ratio, closeTo(1.0, 0.0001));
+    });
+
+    test('throws ArgumentError on zero', () {
+      expect(() => CoffeeTools.ratioCoffeeDM(0), throwsArgumentError);
+    });
+
+    test('throws ArgumentError on negative number', () {
+      expect(() => CoffeeTools.ratioCoffeeDM(-2), throwsArgumentError);
     });
   });
 }
+
